7dc11035b5af0a58d1e3fa013c5bf179312f9ceb
aprilfaithdays <aprilfaith.e@gmail.com>
2020-03-12 11:59:59 -0400

    donee

--- index.js
+++ index.js
@@ -2,5 +2,54 @@ document.addEventListener("DOMContentLoaded", function(){
   createGrid()
   renderBot(currentPosition)
 
-  // ADD CODE HERE!
+  // listener for press a button, move, backspace(delete)
+  // identify item to appened, create, add text to element, append
+  const button = document.getElementById('move-button')
+  const moveTracker = document.getElementById("moves-container");
+  
+  document.addEventListener('keydown', function(event){
+    let direction = event.key
+    let li = document.createElement("li")
+
+    if (direction === "ArrowLeft"){
+      li.innerText = "Left"
+      moveTracker.append(li);
+    };
+    if (direction === "ArrowRight"){
+      li.innerText = "Right"
+      moveTracker.append(li);
+    };
+    if (direction === "ArrowUp"){
+      li.innerText = "Up"
+      moveTracker.append(li);
+    };
+    if (direction === "ArrowDown"){
+      li.innerText = "Down"
+      moveTracker.append(li);
+    }
+    if (direction === "Backspace"){
+      moveTracker.lastChild.remove()
+    }
+
+  })   
+
+
+  button.addEventListener('click', function(event){
+    let li = moveTracker.querySelector("li")
+    
+    if (li.innerText === "Left"){
+      move("left");
+    };
+    if (li.innerText === "Right"){
+      move("right")
+    };
+    if (li.innerText === "Up"){
+      move("up")
+    };
+    if (li.innerText === "Down"){
+      move("down")
+    };
+
+    li.remove()
+  })   
 })
